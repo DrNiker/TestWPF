56bf14a48fe0f080a97484dc05b7aa0963851da1
DrNiker <34690809+DrNiker@users.noreply.github.com>
2023-03-24 11:36:41 +0200

    fix

--- Model/Data.cs
+++ Model/Data.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SQLite;
+using System.Windows;
 
 namespace TestWPF.Model
 {
@@ -28,77 +30,97 @@ namespace TestWPF.Model
         {
             List<Data> data = new List<Data>();
             conn = new SQLiteConnection("Data Source= data.db; Version = 3; New = True; Compress = True; ");
+
+            SQLiteDataReader datareader;
+            SQLiteCommand cmd;
+
             try
             {
                 conn.Open();
+                cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT * FROM DataTable";
+                datareader = cmd.ExecuteReader();
             }
             catch (Exception ex)
             {
+                MessageBox.Show("Database Error: " + ex);
                 Console.WriteLine(ex);
+                conn.Close();
                 return null;
             }
-            SQLiteDataReader datareader;
-            SQLiteCommand cmd;
-            cmd = conn.CreateCommand();
-            cmd.CommandText = "SELECT * FROM DataTable";
 
-            datareader = cmd.ExecuteReader();
             while (datareader.Read())
             {
                 data.Add(new Data(datareader.GetInt32(0), datareader.GetString(1), datareader.GetString(2), datareader.GetString(3)));
             }
             conn.Close();
-            return data ;
+            return data;
         }
         static public string Insert(string _applicationName, string _userName, string _comment)
         {
+            SQLiteCommand cmd;
             try
             {
                 conn.Open();
+                cmd = conn.CreateCommand();
+                cmd.CommandText = $"INSERT INTO DataTable (applicationName, userName, coment) VALUES(@text1, @text2, @text3); ";
+                cmd.Prepare();
+                cmd.Parameters.Add("@text1", DbType.AnsiString).Value = _applicationName;
+                cmd.Parameters.Add("@text2", DbType.AnsiString).Value = _userName;
+                cmd.Parameters.Add("@text3", DbType.AnsiString).Value = _comment;
+                cmd.ExecuteNonQuery();
             }
             catch (Exception ex)
             {
+                MessageBox.Show("Database Error: " + ex);
                 Console.WriteLine(ex);
+                conn.Close();
                 return "Error";
             }
-            SQLiteCommand cmd;
-            cmd = conn.CreateCommand();
-            cmd.CommandText = $"INSERT INTO DataTable (applicationName, userName, coment) VALUES({_applicationName}, {_userName}, {_comment}); ";
-            cmd.ExecuteNonQuery();
+            conn.Close();
             return "Done";
         }
         static public string Update(int _id, string _applicationName, string _userName, string _comment)
         {
+            SQLiteCommand cmd;
             try
             {
                 conn.Open();
+                cmd = conn.CreateCommand();
+                cmd.CommandText = $"UPDATE DataTable SET applicationName = @text1, userName = @text2, coment = @text3 WHERE id = {_id}; ";
+                cmd.Prepare();
+                cmd.Parameters.Add("@text1", DbType.AnsiString).Value = _applicationName;
+                cmd.Parameters.Add("@text2", DbType.AnsiString).Value = _userName;
+                cmd.Parameters.Add("@text3", DbType.AnsiString).Value = _comment;
+                cmd.ExecuteNonQuery();
             }
             catch (Exception ex)
             {
+                MessageBox.Show("Database Error: " + ex);
                 Console.WriteLine(ex);
+                conn.Close();
                 return "Error";
             }
-            SQLiteCommand cmd;
-            cmd = conn.CreateCommand();
-            cmd.CommandText = $"UPDATE DataTable SET applicationName = {_applicationName}, userName = {_userName}, coment = {_comment} WHERE id = {_id}; ";
-            cmd.ExecuteNonQuery();
+            conn.Close();
             return "Done";
         }
         static public string Delete(int _id)
         {
+            SQLiteCommand cmd;
             try
             {
                 conn.Open();
+                cmd = conn.CreateCommand();
+                cmd.CommandText = $"DELETE FROM DataTable WHERE id = {_id}; ";
+                cmd.ExecuteNonQuery();
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
+                conn.Close();
                 return "Error";
             }
-            SQLiteCommand cmd;
-            cmd = conn.CreateCommand();
-            cmd.CommandText = $"DELETE FROM DataTable WHERE id = {_id}; ";
-            cmd.ExecuteNonQuery();
+            conn.Close();
             return "Done";
         }
     }
